4e3b8f55d2c5ea326934e974a9460c42bd947396
henrycheung32 <henry.cheung1972@gmail.com>
2024-10-27 06:46:51 -0400

    Almost done

--- src/server.py
+++ src/server.py
@@ -1,32 +1,32 @@
-from flask import Flask, request, jsonify, send_file
-from flask_cors import CORS
-import base64
-import os
+# from flask import Flask, request, jsonify, send_file
+# from flask_cors import CORS
+# import base64
+# import os
 
 
-app = Flask(__name__)
-CORS(app)
+# app = Flask(__name__)
+# CORS(app)
 
 
-UPLOAD_FOLDER = 'src\\uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+# UPLOAD_FOLDER = 'src\\uploads'
+# os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@app.route('/upload_image', methods=['POST'])
-def upload_image():
+# @app.route('/upload_image', methods=['POST'])
+# def upload_image():
     
-    # Ensure image data is provided
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image data found'}), 400
+#     # Ensure image data is provided
+#     if 'image' not in request.files:
+#         return jsonify({'error': 'No image data found'}), 400
     
-    image_file = request.files['image']
+#     image_file = request.files['image']
 
-    save_path = os.path.join(UPLOAD_FOLDER, "received_image.png")
+#     save_path = os.path.join(UPLOAD_FOLDER, "received_image.png")
 
-    image_file.save(save_path)
-    print(image_file)
+#     image_file.save(save_path)
+#     print(image_file)
     
-    return jsonify({'message': 'image saved successfully!'}), 200
+#     return jsonify({'message': 'image saved successfully!'}), 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
