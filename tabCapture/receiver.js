80f0babc9a025e9d8f12e9282bb9fdc2cc69b9ff
ev3rshade <hskeney@gmail.com>
2024-10-27 03:13:54 -0400

    updated the fetch function

--- tabCapture/receiver.js
+++ tabCapture/receiver.js
@@ -157,9 +157,8 @@ function grabFrame1() {
 function sendData() {
   const base64Image = canvasA.toDataURL('image/pmg')
   let response = fetch( "http://127.0.0.1:5000/upload_image", {
-      url: '/upload_image',
-      type: 'POST',
-      contentType: 'application/json',
+      //url: '/upload_image',
+      method: 'POST',
       data: JSON.stringify({ 'image': base64Image}),
       success: function(response) {
         console.log("success")  
