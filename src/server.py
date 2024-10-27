1053d70d5da841e3c91dca567c78fc906905785a
ev3rshade <hskeney@gmail.com>
2024-10-27 04:18:13 -0400

    fixed flask server

--- src/server.py
+++ src/server.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import base64
-import numpy as np
-from PIL import Image
-from io import BytesIO
+
+
 
 app = Flask(__name__)
 CORS(app)
@@ -18,22 +17,10 @@ def upload_image():
         return jsonify({'error': 'No image data found'}), 400
     
     # Get the base64 image string
-    image_b64 = data['image']
+    image_b64 = data
     
-    # Convert Base64 string to a NumPy array
-    np_image = convert_base64_to_uint8(image_b64)
-    
-    # For example purposes, we can return the shape of the array
-    return jsonify({'shape': np_image.shape})
-
-def convert_base64_to_uint8(image_b64):
-    # Decode the Base64 string
-    image_data = base64.b64decode(image_b64.split(',')[1])  # Split at comma if it contains data type prefix
-    image = Image.open(BytesIO(image_data)).convert('RGB')
-    
-    # Convert image to NumPy array
-    np_image = np.array(image, dtype=np.uint8)
-    return np_image
+    return jsonify(image_b64)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
